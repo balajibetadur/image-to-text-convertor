2229af9122d24d9cc590e3f8d3c52c25263aada5
balajibetadur <balajibetadur@gmail.com>
2019-11-06 16:03:49 +0530

    image-to_text

---
+++ image_to_text.py
@@ -0,0 +1,10 @@
+# import pytesseract
+# from PIL import Image
+
+
+
+# pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+# img = Image.open('ig.jpg')
+
+# text = pytesseract.image_to_string(img)
+
